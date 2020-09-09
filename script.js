c2f6ccdafaba542cccf91a903325ef3595e33cf9
Glitch (lost-art-inspo) <none>
2020-09-09 10:05:55 +0000

    Version 3

--- script.js
+++ script.js
@@ -5,14 +5,13 @@
 console.log("hi");
 
 function homeBttn(){
-  
-  
+  document.getElementById("LndPge").style.display="block";
+  document.getElementById("DrwPge").style.display="none";
 }
 
 function drawingPgeChng1(){
-  
-  
-  
+  document.getElementById("DrwPge").style.display="block";
+  document.getElementById("LndPge").style.display="none"; 
 }
 
 function paintingPgeChng1(){
